b9685b79b0a0ce6baf8c6a87e2841c9bb3575c2b
ShakthiM91 <coolgang.shakthi@gmail.com>
2014-02-16 00:33:11 +0530

    add password

--- src/com/boston/shakthi/playText.java
+++ src/com/boston/shakthi/playText.java
@@ -2,6 +2,8 @@ package com.boston.shakthi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,9 +18,22 @@ public class playText extends Activity{
 		setContentView(R.layout.text);
 		
 		Button checkCommand = (Button)findViewById(R.id.btn_checkCommand);
-		ToggleButton mkPass = (ToggleButton)findViewById(R.id.tb_password);
-		EditText cmd = (EditText)findViewById(R.id.et_commands);
+		final ToggleButton mkPass = (ToggleButton)findViewById(R.id.tb_password);
+		final EditText cmd = (EditText)findViewById(R.id.et_commands);
 		TextView results = (TextView)findViewById(R.id.txt_result);
+		mkPass.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(mkPass.isChecked()){
+					cmd.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				}
+				else{
+					cmd.setInputType(InputType.TYPE_CLASS_TEXT);
+				}
+			}
+		});
 	}
 
 }
